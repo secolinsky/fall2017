ab4650e06ed805d455d944e3705b30c15fcd00aa
Maurice Ticas <maurice.ticas@gmail.com>
2018-02-05 15:44:47 -0800

    semester ended

--- math119/weather_data.py
+++ math119/weather_data.py
@@ -3,11 +3,20 @@ from bs4 import BeautifulSoup
 from urlparse import urljoin
 import re
 
-page='https://www.wunderground.com/history/airport/MSSS/2016/10/11/MonthlyCalendar.html?req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo=&MR=1'
+page='https://www.wunderground.com/history/airport/MSSS/2016/10/13/MonthlyCalendar.html?req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo='
 c=urllib2.urlopen(page)
 soup=BeautifulSoup(c.read())
 
-days=[]
+data = soup.find_all('span','high')
+col =  [e.text for e in data]
 
-for n in soup.td:
-    print n
+col=[float(e.replace(u"\xb0","")) for e in col]
+col = col[0::2]
+
+for i in range(31):
+    print col[i]
+
+# for n in soup('span'):
+#     print n
+
+
